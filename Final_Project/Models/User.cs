fea18941f9a16e65bc548b5c2d642723d002a159
Nazanin11 <152808920+Nazanin11@users.noreply.github.com>
2024-07-07 13:24:19 +0330

    Update User.cs

--- Final_Project/Models/User.cs
+++ Final_Project/Models/User.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using System.IO;
 ﻿
 namespace ApProject.Models
 {
@@ -24,6 +26,96 @@ namespace ApProject.Models
             }
             return null;
         }
+
+        public static bool LoadFromJsonFile()
+{
+
+    try
+    {
+
+        if (File.Exists("C:\\c#\\ApProject\\User.txt"))
+        {
+            string json = File.ReadAllText("C:\\c#\\ApProject\\User.txt");
+            User.Users = JsonConvert.DeserializeObject<List<User>>(json);
+            if(User.Users == null)
+            {
+                User.Users = new List<User>();
+            }
+        }
+        if (File.Exists("C:\\c#\\ApProject\\Admin.txt"))
+        {
+            string json = File.ReadAllText("C:\\c#\\ApProject\\Admin.txt");
+            Admin.Admins = JsonConvert.DeserializeObject<List<Admin>>(json);
+            if(Admin.Admins == null)
+            {
+                Admin.Admins= new List<Admin>();
+            }
+            
+        }
+        if (File.Exists("C:\\c#\\ApProject\\Customer.txt"))
+        {
+            string json = File.ReadAllText("C:\\c#\\ApProject\\Customer.txt");
+            Customer.Customers = JsonConvert.DeserializeObject<List<Customer>>(json);
+            if(Customer.Customers == null)
+            {
+                Customer.Customers= new List<Customer>();
+            }
+        }
+        if (File.Exists("C:\\c#\\ApProject\\Restaurant.txt"))
+        {
+            string json = File.ReadAllText("C:\\c#\\ApProject\\Restaurant.txt");
+            Resturant.Resturants = JsonConvert.DeserializeObject<List<Resturant>>(json);
+            if(Resturant.Resturants== null)
+            {
+                Resturant.Resturants= new List<Resturant>();
+            }
+        }
+        if (File.Exists("C:\\c#\\ApProject\\Complaint.txt"))
+        {
+            string json = File.ReadAllText("C:\\c#\\ApProject\\Complaint.txt");
+            Complaint.Complaints = JsonConvert.DeserializeObject<List<Complaint>>(json);
+            if(Complaint.Complaints == null)
+            {
+                Complaint.Complaints= new List<Complaint>();
+            }
+        }
+        return true;
+    }
+    catch(Exception ex)
+    {
+        return false;
+
+    }
+
+
+}
+public static bool SaveToJsonFile()
+{
+    try
+    {
+        string json1 = JsonConvert.SerializeObject(User.Users);
+        File.WriteAllText("C:\\c#\\ApProject\\User.txt", json1);
+
+        string json2 = JsonConvert.SerializeObject(Admin.Admins);
+        File.WriteAllText("C:\\c#\\ApProject\\Admin.txt", json2);
+
+        string json3 = JsonConvert.SerializeObject(Customer.Customers);
+        File.WriteAllText("C:\\c#\\ApProject\\Customer.txt", json3);
+
+        string json4 = JsonConvert.SerializeObject(Resturant.Resturants);
+        File.WriteAllText("C:\\c#\\ApProject\\Restaurant.txt", json4);
+
+        string json5 = JsonConvert.SerializeObject(Complaint.Complaints);
+        File.WriteAllText("C:\\c#\\ApProject\\Complaint.txt", json5);
+
+        return true;
+    }
+    catch(Exception ex)
+    {
+        return false;
+    }
+
+}
      
     }
 }
