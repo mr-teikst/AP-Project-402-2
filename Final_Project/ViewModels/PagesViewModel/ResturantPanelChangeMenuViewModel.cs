9b664b76d650ce0894e1c1c06ff62e1517354a06
mr-teikst <erfan.teist2027@gmail.com>
2024-07-07 14:37:22 +0330

    resturant delete and edit food feature

--- Final_Project/ViewModels/PagesViewModel/ResturantPanelChangeMenuViewModel.cs
+++ Final_Project/ViewModels/PagesViewModel/ResturantPanelChangeMenuViewModel.cs
@@ -80,6 +80,84 @@ namespace Final_Project.ViewModels.PagesViewModel
         });
 
 
+        public RelayCommand DeleteFoodBTNCommand => new RelayCommand(execute =>
+        {
+            FoodMenuItemViewModel? FoodMenuItemVM;
+            try
+            {
+                FoodMenuItemVM = SelectedItemField.DataContext as FoodMenuItemViewModel;
+                if (FoodMenuItemVM == null) return;
+            }
+            catch
+            {
+                return;
+            }
+            ResturantPanelViewModel.MainResturant.Foods.Remove(FoodMenuItemVM.MainFood);
+            BuildCollection(ResturantPanelViewModel.MainResturant.Foods);
+        });
+
+
+        public RelayCommand EditFoodBTNCommand => new RelayCommand(execute =>
+        {
+            Food food;
+            try
+            {
+                FoodMenuItemViewModel? FoodMenuItemVM = SelectedItemField.DataContext as FoodMenuItemViewModel;
+                if (FoodMenuItemVM == null) return;
+                food = FoodMenuItemVM.MainFood;
+            }
+            catch
+            {
+                return;
+            }
+            AddNewFoodPopUp pw = new AddNewFoodPopUp();
+            AddNewFoodPopUpViewModel pwVM = new AddNewFoodPopUpViewModel(pw);
+            pw.DataContext = pwVM;
+
+            pwVM.NameField = food.Name;
+            pwVM.MaterialField = food.Material;
+            pwVM.PriceField = food.Price;
+            pwVM.CountField = food.Count;
+            pwVM.ImageFileField = food.Image;
+
+            pwVM.OkTask = () =>
+            {
+                string errormsg = "";
+                if (pwVM.NameField != null)
+                {
+                    food.ChangeName(pwVM.NameField);
+                }
+                if (pwVM.MaterialField != null)
+                {
+                    food.ChangeMaterial(pwVM.MaterialField);
+                }
+                if (pwVM.PriceField != null && Food.ValidatePrice(pwVM.PriceField))
+                {
+                    food.ChangePrice(pwVM.PriceField);
+                }
+                else if(pwVM.PriceField != null)
+                {
+                    errormsg += "Invalid Price ,";
+                }
+                if (pwVM.CountField != null && pwVM.CountField > 0)
+                {
+                    food.ChangeCount(pwVM.CountField);
+                }
+                else if(pwVM.CountField != null)
+                {
+                    errormsg += "Invalid Count ";
+                }
+                if (pwVM.ImageFileField != null)
+                {
+                    food.ChangeImage(pwVM.ImageFileField);
+                }
+                pwVM.HintField = errormsg;
+                BuildCollection(ResturantPanelViewModel.MainResturant.Foods);
+                pw.Close();
+            };
+            pw.ShowDialog();
+        });
+
         private void BuildCollection(List<Food> foods)
         {
             ItemSourceField.Clear();
